bf514828705ac8cc4df54303bdb4d77b1fb49c18
Olivier Adjoua <passi.adjoua@upmc.fr>
2022-06-03 19:02:59 +0200

    Updated 'tinker_types.h'

--- GPU/source/tinker_types.h
+++ GPU/source/tinker_types.h
@@ -20,4 +20,33 @@
 #    define ener_rtyp integer(8)
 #    define MPI_MDTYP MPI_INTEGER8
 #  endif
+
+#define __concat(x,y) x ## y
+#define __concat2(x,y,z) x ## y ## z
+#define __char(x) #x
+
+#define __Cat(x,y) __concat(x,y)
+#define __Cat2(x,y,z) __concat2(x,y,z)
+#define __cuCat2(x,y,z) __concat2(x,y,z)
+#define __Char(x) __char(x)
+
+#define __TINKER_FATAL__ call fatal1(__FILE__,__LINE__)
+
+#define __use_grd__   1
+#define __use_ene__   2
+#define __use_vir__   4
+#define __use_act__   8
+#define __use_sca__  16
+
+#define __use_mpi__          1
+#define __radepsOpt__        2
+#define __use_softcore__     4
+#define __use_groups__       8
+#define __use_gamd__        64
+#define __use_polymer__    128
+#define __use_shortRange__ 256
+#define __use_longRange__  512
+#define __use_ewald__     1024
+#define __use_lambdadyn__ 2048
+
 #endif
