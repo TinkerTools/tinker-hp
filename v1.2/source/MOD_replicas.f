8126bec590eaac295b37036972dc10e76d8abe61
louis.lagardere@gmail.com <louis.lagardere@sorbonne-universite.fr>
2022-10-31 09:18:09 +0100

    added MOD_replicas.f to sources

---
+++ v1.2/source/MOD_replicas.f
@@ -0,0 +1,26 @@
+c
+c     Sorbonne University
+c     Washington University in Saint Louis
+c     University of Texas at Austin
+c
+c     #####################################################################
+c     ##                                                                 ##
+c     ##  module replicass   --  replicas variables                      ##
+c     ##                                                                 ##
+c     #####################################################################
+c
+c     nreps: number of replicas
+c     rank_reploc: index of the local replica
+c     COMM_ROOT2ROOT: root to roor communicator
+c     use_repls: logical flag to determine if multiple replicas are used
+c
+c
+      module replicas
+      implicit none
+      integer :: nreps
+      integer :: rank_reploc
+      integer :: COMM_ROOT2ROOT
+      logical :: use_reps
+      real*8, allocatable :: lambdastart(:)
+      save
+      end module replicas
