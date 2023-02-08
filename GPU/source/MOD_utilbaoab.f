75252041c9f7da6cded810593b9b8c0340f3d62a
Olivier Adjoua <passi.adjoua@upmc.fr>
2023-02-08 15:05:46 +0100

    Added 'MOD_utilbaoab.f'

---
+++ GPU/source/MOD_utilbaoab.f
@@ -0,0 +1,28 @@
+#include "tinker_macro.h"
+      module utilbaoab
+      implicit none
+
+      interface
+      module subroutine apply_b_piston(dt,pres)
+      real(r_p), intent(in) :: dt,pres
+      end subroutine
+
+      module subroutine apply_o_piston(dt)
+      real(r_p), intent(in) :: dt
+      end subroutine
+
+      module subroutine apply_a_piston(dt,istep,A_full)
+      integer  ,intent(in):: istep
+      logical  ,intent(in):: A_full
+      real(r_p),intent(in):: dt
+      end subroutine
+
+      module subroutine set_langevin_thermostat_coeff(dta)
+      real(r_p),intent(in):: dta
+      end subroutine
+
+      module subroutine apply_langevin_thermostat
+      end subroutine
+      end interface
+
+      end module utilbaoab
